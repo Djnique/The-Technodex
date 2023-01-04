393050b9be628b9d0a1397c28d58579f6f0e78d7
Djnique <djnique.god@icloud.com>
2023-01-04 12:00:55 -0600

    Update pong-classes.js

--- Javascript/pong-classes.js
+++ Javascript/pong-classes.js
@@ -40,10 +40,10 @@ export class Ball {
   ) {
     this.size = { x: 20, y: 20 }; // default ball size
     this.position = { x: 50, y: 50 }; // default ball position
-    this.direction = { x: 1, y: 0 }; //Ball speed when game starts
-    this.acceleration = -7; //X speed multipler on successful block
-    this.initial = { x: 0, y: 0 }; //speed reset on point score
-    this.maximum = { x: 10, y: 5}; //maximum speed
+    this.direction = { x: 5, y: 0 }; //Ball speed when game starts
+    this.acceleration = 0; //X speed multipler on successful block
+    this.initial = { x: 2, y: 0 }; //speed reset on point score
+    this.maximum = { x: 20, y: 10}; //maximum speed
     this.inertialTransfer = 0.25; //multiplier for velocity.y transfer from paddle
     this.htmlElement = null; //element on HTML page
   }

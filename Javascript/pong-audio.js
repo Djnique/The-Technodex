1ae58b10115626de7a623501575dee3a27451e1e
Djnique <djnique.god@icloud.com>
2023-01-09 12:51:48 -0600

    Update pong-audio.js

--- Javascript/pong-audio.js
+++ Javascript/pong-audio.js
@@ -55,7 +55,7 @@ soundArray.push(scoreSound);
 export var ambientSound = new soundFile("Adventure_Meme.mp3");
 soundArray.push(ambientSound);
 ambientSound.player.loop = true; //turn on looping
-ambientSound.player.volume.value = -20; //turn down volume
+ambientSound.player.volume.value = 5; //turn down volume
 
 export var adventureMusic = new soundFile("Eight_Bit_Village_Loop.mp3");
 soundArray.push(adventureMusic);

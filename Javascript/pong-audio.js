b09438f6f2b250308d3d1cc6442123f4c32c02c8
Djnique <djnique.god@icloud.com>
2023-01-09 12:25:57 -0600

    Update pong-audio.js

--- Javascript/pong-audio.js
+++ Javascript/pong-audio.js
@@ -8,7 +8,7 @@ class soundFile {
   constructor(file, deferPlay) {
     //this.deferPlay = false;
     this.player = new Tone.Player({
-      url: "../Sounds/" + file,
+      url: "./Sounds/" + file,
       loop: false,
       autostart: false
     }).toMaster();
